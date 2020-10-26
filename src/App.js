3ffe97b6c218cc05a0ffab45d4ecc38d3835eaf9
Hiranai, Yuko <yuko.hiranai@rakuten.com>
2020-10-26 09:58:15 +0900

    create my own React boilerpalte

--- src/App.js
+++ src/App.js
@@ -1,17 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-      </header>
-    </div>
-  );
+  return <div><h1>Hello world</h1></div>
 }
 
 export default App;
