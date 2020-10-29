5a7dcd8f0121b0bb17bf75fb6f6490a4c42b8633
Hiranai, Yuko <yuko.hiranai@rakuten.com>
2020-10-29 09:19:44 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div><h1>Hello world</h1></div>
+// function App() {
+//   return <div><h1>Hello world</h1></div>
+// }
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
